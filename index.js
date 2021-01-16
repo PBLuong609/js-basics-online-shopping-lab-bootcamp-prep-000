8180abc83f83031953dcaf7450d2ffaf419762a8
Philip Luong <pbluong@gmail.com>
2021-01-16 22:47:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,51 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemPrice = Math.floor(Math.random()*100);
+ var itemName = item;
+ var tempCart={};
+ tempCart[itemName] = itemPrice;
+
+ cart.push(tempCart);
+ console.log("addToCart function called:");
+ console.log(tempCart);
+ console.log(cart);
+
+ return cart;
 }
+addToCart('bananas');
+//addToCart('apples');
+
 
 function viewCart() {
   // write your code here
+  var cartIntroMsg = "In your cart, you have ";
+  var cartBodyMsg;
+
+
+  console.log(cart.length);
+  if(cart.length == 0){
+    console.log("Your shopping cart is empty.");
+    return "Your shopping cart is empty.";
+  }
+  else if (cart.length == 1) {
+    for (var i = 0; i < cart.length; i++) {
+      var str = "I am me";
+      str.replace("am","not");
+      //var str = cart[i];
+      console.log(`${cartIntroMsg} ${str}.`);
+      //str.replace(' : ',' at $');
+    }
+  }
+  else if (cart.length == 2) {
+
+  }
+  else {
+
+  }
+
 }
+viewCart();
 
 function total() {
   // write your code here
